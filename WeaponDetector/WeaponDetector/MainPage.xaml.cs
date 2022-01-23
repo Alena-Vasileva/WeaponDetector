12b72d0ea2d816d969a38e083ca7b1e9576c81e5
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-01-23 15:25:11 +0300

    init commit

---
+++ WeaponDetector/WeaponDetector/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+using Xamarin.Essentials;
+
+namespace WeaponDetector
+{
+    public partial class MainPage : ContentPage
+    {
+        private static Image img;
+
+        public static Image Photo
+        {
+            get => img;
+        }
+
+        public static string PhotoPath { get; set; }
+
+        Button takePhotoBtn;
+        Button getPhotoBtn;
+        Button detect;
+
+        public MainPage()
+        {
+            //InitializeComponent();
+            takePhotoBtn = new Button { Text = "Сделать фото" };
+            getPhotoBtn = new Button { Text = "Выбрать фото" };
+            detect = new Button { Text = "Найти оружие" };
+            img = new Image();
+
+            // выбор фото
+            getPhotoBtn.Clicked += GetPhotoAsync;
+
+            // съемка фото
+            takePhotoBtn.Clicked += TakePhotoAsync;
+
+            detect.Clicked += DetectAsync;
+            img.VerticalOptions = LayoutOptions.Start;
+            detect.Padding = 20;
+            detect.Margin = 0;
+            detect.CornerRadius = 10;
+            detect.BackgroundColor = Color.FromHex("c5d0e6");
+            takePhotoBtn.BackgroundColor = Color.FromHex("d1f7ff");
+            getPhotoBtn.BackgroundColor = Color.FromHex("d1f7ff");
+            takePhotoBtn.Padding = 20;
+            getPhotoBtn.Padding = 20;
+            takePhotoBtn.CornerRadius = 10;
+            getPhotoBtn.CornerRadius = 10;
+            takePhotoBtn.Margin = new Thickness(0, 0, 3, 0);
+            getPhotoBtn.Margin = new Thickness(3, 0, 0, 0);
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                WidthRequest = Width,
+                BackgroundColor = Color.FromHex("#025669"),
+                Padding = 10,
+                Children = {
+                    new StackLayout
+                    {
+                         Padding = 5,
+                         Children = {takePhotoBtn, getPhotoBtn},
+                         Orientation = StackOrientation.Horizontal,
+                         HorizontalOptions = LayoutOptions.CenterAndExpand
+                    },
+                    detect,
+                    img
+                }
+            };
+        }
+
+        async void GetPhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                // выбираем фото
+                var photo = await MediaPicker.PickPhotoAsync();
+                // загружаем в ImageView
+                img.Source = ImageSource.FromFile(photo.FullPath);
+                PhotoPath = photo.FullPath;
+                img.HeightRequest = Content.Height - detect.Height * 2 - 45;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        async void TakePhotoAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                {
+                    Title = $"xamarin.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.png"
+                });
+
+                // для примера сохраняем файл в локальном хранилище
+                var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+                using (var stream = await photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(newFile))
+                    await stream.CopyToAsync(newStream);
+
+                // загружаем в ImageView
+                img.Source = ImageSource.FromFile(photo.FullPath);
+                PhotoPath = photo.FullPath;
+                img.HeightRequest = Content.Height - detect.Height * 2 - 45;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+
+        async void DetectAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                PhotoManager.Transfer();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Сообщение об ошибке", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ WeaponDetector/WeaponDetector/PhotoManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeaponDetector;
+using Amazon.CognitoIdentity;
+using Amazon;
+using Amazon.Auth;
+using Amazon.Internal;
+using Amazon.Runtime;
+using Amazon.Util;
+using System.IO;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using Android.Graphics;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using Amazon.S3.Model;
+
+
+namespace WeaponDetector
+{
+    internal static class PhotoManager
+    {
+
+        public static void Transfer()
+        {
+            Debug.WriteLine("Start");
+            var s3Client = new AmazonS3Client(
+                "AKIAZLSCHF3AL4A45KWU",
+                "hm5TLaBiTCE75sYNJcZhGoAqN1GKvQ65cwZWUpSk",
+                RegionEndpoint.EUCentral1
+            );
+            Debug.WriteLine("Made file1111111111");
+            var transferUtility = new TransferUtility(s3Client);
+            Debug.WriteLine("Start file");
+            Random rnd = new Random();
+            var backingFile = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), rnd.Next(10000, 100000) + ".jpeg");
+            Debug.WriteLine("Made file");
+            transferUtility.Upload(MainPage.PhotoPath, "guns1g23");
+            Debug.WriteLine("Downloaded");
+        }
+    }
+}
